ac81ecea160d8d6f00b18a3f234b3e82af7d41a0
Ali <ali.arioglu06@gmail.com>
2023-08-30 18:46:05 +0300

    conversations setup

---
+++ lib/conversation.ts
@@ -0,0 +1,70 @@
+import { db } from "@/lib/db";
+
+// ilk basta 2 memberin birbiriyle conversationu var mı diye bakılıyor yoksa create ediliyor.
+
+export const getOrCreateConversation = async (
+  memberOneId: string,
+  memberTwoId: string
+) => {
+  let conversation =
+    (await findConversation(memberOneId, memberTwoId)) ||
+    (await findConversation(memberTwoId, memberOneId));
+
+  if (!conversation) {
+    conversation = await createNewConversation(memberOneId, memberTwoId);
+  }
+
+  return conversation;
+};
+
+const findConversation = async (memberOneId: string, memberTwoId: string) => {
+  try {
+    return await db.conversation.findFirst({
+      where: {
+        AND: [{ memberOneId: memberOneId }, { memberTwoId: memberTwoId }],
+      },
+      include: {
+        memberOne: {
+          include: {
+            profile: true,
+          },
+        },
+        memberTwo: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+  } catch (error) {
+    return null;
+  }
+};
+
+const createNewConversation = async (
+  memberOneId: string,
+  memberTwoId: string
+) => {
+  try {
+    return await db.conversation.create({
+      data: {
+        memberOneId,
+        memberTwoId,
+      },
+      include: {
+        memberOne: {
+          include: {
+            profile: true,
+          },
+        },
+        memberTwo: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+  } catch (error) {
+    return null;
+  }
+};
