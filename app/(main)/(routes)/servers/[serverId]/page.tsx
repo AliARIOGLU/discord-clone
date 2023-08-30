3d269af0911ce7ef7fbae4fd733ee28e30ec8624
Ali <ali.arioglu06@gmail.com>
2023-08-30 15:27:00 +0300

    channel id page initialized.

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,5 +1,53 @@
-const ServerIdPage = () => {
-  return <div>ServerIdPage</div>;
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  };
+}
+
+const ServerIdPage: React.FC<ServerIdPageProps> = async ({ params }) => {
+  const profile = await currentProfile();
+
+  if (!profile) return redirectToSignIn();
+
+  // alttaki sorgu sayfa ilk yüklendiğinde her zaman general channel gösterilsin diye.
+  // navigation sidebarda kendi kanalıma tıkladığımda beni buraya redirect etmesi için
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  // teknik olarak bunun olması imkansız ama koruma için koydum
+  if (initialChannel?.name !== "general") {
+    return null;
+  }
+
+  return redirect(
+    `/servers/${params?.serverId}/channels/${initialChannel?.id}`
+  );
 };
 
 export default ServerIdPage;
